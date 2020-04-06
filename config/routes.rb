fdaf170307df72d985cb015fe3ba793d51e57a00
Phil Billingsby <philbillingsby@gmail.com>
2020-04-05 23:00:25 -0400

    Initial commit.

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions
+  root 'users#index'
+  get '/login', to: 'sessions#new'
+end
