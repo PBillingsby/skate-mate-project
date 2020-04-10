f8b4d95569699747365f814371967809619f1e49
Phil Billingsby <philbillingsby@gmail.com>
2020-04-10 16:11:27 -0400

    Change user new.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,5 @@
 class Location < ApplicationRecord
   has_many :users
-  geocoded_by :full_address
+  geocoded_by :address
   after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
-  def full_address
-    [city, country, longitude, latitude].compact.join(', ')
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
   validates_confirmation_of :password
-  belongs_to :location
-  accepts_nested_attributes_for :location
 end
