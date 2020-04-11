95780fa4241b3b7515f426023db121ce95b90773
Phil Billingsby <philbillingsby@gmail.com>
2020-04-11 13:56:11 -0400

    Add user nav-bar links.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,5 @@ class ApplicationController < ActionController::Base
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
-  private
-  def current_user
-    byebug
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in
-    session[:current_user]
-  end
+  
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,12 @@ module UsersHelper
       link_to "#{current_user.location.city}, #{current_user.location.country}", location_path(current_user.location)
     end
   end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in
+    byebug
+    session[:current_user]
+  end
 end
