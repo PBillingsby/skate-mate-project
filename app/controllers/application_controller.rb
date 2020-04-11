6ea14fdbf5abe74be26ef6e52dabf46b195c3df4
Phil Billingsby <philbillingsby@gmail.com>
2020-04-11 13:46:33 -0400

    Add user navigation partial view.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,12 @@ class ApplicationController < ActionController::Base
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
   private
-
   def current_user
+    byebug
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in
+    session[:current_user]
   end
 end
