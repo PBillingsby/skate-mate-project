1893ad676d4589aede9d55638347cf9a3614accc
Phil Billingsby <philbillingsby@gmail.com>
2020-04-15 21:34:48 -0400

    Add nested routes for check_in.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,7 +9,7 @@ module UsersHelper
   end
   def user_location
     if !@user.check_in.empty?
-      link_to "#{@user.check_in.last.location.city}"
+      link_to "#{@user.check_in.last.location.city}", user_check_in_path(@user)
     else
       "Well, nowhere! Head over to add your location..."
     end
