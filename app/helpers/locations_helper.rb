5d15a546f2a27cc9db9e6aff32aa1f9439876f77
Phil Billingsby <philbillingsby@gmail.com>
2020-04-17 12:24:26 -0400

    Change spot form to add file_field for :image.

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -10,8 +10,9 @@ module LocationsHelper
       redirect_to user_path(user)
     else
       location_search = Geocoder.search(location_params[:city]).first
-      @location = Location.find_or_create_by(city: location_search.city, country: location_search.country)
+      @location = Location.find_or_create_by(city: location_params.city, country: location_search.country)
       if @location
+        byebug
         new_check_in = @location.check_ins.find_or_create_by(user_id: user.id)
         flash[:success] = "Location Changed!"
         redirect_to user_path(user)
--- app/models/location.rb
+++ app/models/location.rb
@@ -4,5 +4,4 @@ class Location < ApplicationRecord
   has_many :spots
   geocoded_by :city
   after_validation :geocode, if: ->(obj){ obj.city.present? and obj.city_changed? }
-  
 end
