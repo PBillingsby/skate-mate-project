3fb7620320e51749bec5cdf0934ceb67c346595f
Phil Billingsby <philbillingsby@gmail.com>
2020-04-19 07:10:31 -0400

    Add footer. Add comments to spot#show.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   has_many :locations, through: :check_ins
   has_many :spots
   has_many :comments
-  has_many :comments, through: :spots
+  # has_many :comments, through: :spots
 
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, :on => :create, confirmation: true, if: -> { password.present? }
