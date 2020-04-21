963491e4b350547f0aab8d296346c5fb369391fb
Phil Billingsby <philbillingsby@gmail.com>
2020-04-21 19:24:22 -0400

    Fix spot_search method.

--- app/helpers/spots_helper.rb
+++ app/helpers/spots_helper.rb
@@ -3,6 +3,7 @@ module SpotsHelper
     if !params[:spot].nil?
       if !spot_params[:location_id].blank?
         @spots = Spot.where(location_id: spot_params[:location_id])
+        flash[:alert] = "<%=link_to "Go Back", spots_path%>".html_safe
         render :index
       else
         @spots = Spot.all
