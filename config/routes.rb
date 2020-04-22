f6d0d0490d8c20e16668ad011beea294d59ccd37
Phil Billingsby <philbillingsby@gmail.com>
2020-04-22 11:43:27 -0400

    Add nested routes for users spots#new.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy, :omniauth]
   resources :users, only: [:new, :create, :show, :edit, :update] do
     resources :check_ins, only: [:new, :destroy]
-    resources :spots, only: [:index, :show]
+    resources :spots, only: [:index, :show, :new]
   end
   resources :locations, only: [:create, :show, :index] do
     resources :spots, only: [:show, :index, :new]
