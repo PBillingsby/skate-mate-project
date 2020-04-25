d9da4a9fc059f80d0c65ea3e77beee38c4c2b4b3
Phil Billingsby <philbillingsby@gmail.com>
2020-04-25 12:43:55 -0400

    Change Css style

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,3 @@
 Location.create(city: "Philadelphia", country: "United States of America")
 Location.create(city: "New York City", country: "United States of America")
 Location.create(city: "Bondi Beach", country: "Australia")
-Spot.create(name: "FDR Skatepark", address: "Pattinson Avenue", user_id: 1, location_id: 1, image: File.open(File.join(Rails.root,'app/assets/images/fdr.jpeg')))
-Spot.create(name: "LES Skatepark", address: "62 Monroe St", user_id: 1, location_id: 2, image: File.open(File.join(Rails.root,'app/assets/images/newLESskatePark_sm.jpg')))
-Spot.create(name: "FDR Skatepark", address: "Pattinson Avenue", user_id: 1, location_id: 3, image: File.open(File.join(Rails.root,'app/assets/images/bondimcredo1.jpg')))
