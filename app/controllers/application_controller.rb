22c13a2474cfe9e9044b8585746a4760ce27b8bf
Phil Billingsby <philbillingsby@gmail.com>
2020-04-27 11:57:58 -0400

    Fix logged_in? method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  def logged_in
-    session[:current_user]
+  def logged_in?
+    !!session[:user_id]
   end
 
   def current_user_path
