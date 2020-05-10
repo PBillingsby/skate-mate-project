2b50f22117fa1ce10c1998cca1253ba1b80c4750
Phil Billingsby <philbillingsby@gmail.com>
2020-05-10 17:41:14 -0400

    Add javascript to click image

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,11 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+document.addEventListener('DOMContentLoaded', function(){
+  let imageShow = document.querySelector('img');
+  imageShow.addEventListener('click', (e) => {
+    e.target.style.width = "70vw";
+    // e.target.className = "";
+  })
+  
+})
