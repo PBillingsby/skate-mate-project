5987b943dbd2eb9a12f3a436252e6ca89a412767
PBillingsby <philbillingsby@gmail.com>
2020-10-11 10:49:56 -0700

    Fix styling on form and update gemfile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,16 +14,21 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-document.addEventListener('DOMContentLoaded', function(event){
-  let imageShow = document.querySelector('img');
-  if (imageShow) { 
-    imageShow.addEventListener('click', (e) => {
+document.addEventListener("DOMContentLoaded", function(event) {
+  let imageShow = document.querySelector("img");
+  if (imageShow) {
+    imageShow.addEventListener("click", e => {
       if (e.target.style.width != "50vw") {
         e.target.style.width = "50vw";
-      }
-      else {
+      } else {
         e.target.style.width = "35vw";
       }
-    })
+    });
   }
-})
+  let stars = document.getElementsByClassName("glyphicon");
+  for (let i = 0; i < stars.length; i++) {
+    stars[i].addEventListener("click", e => {
+      e.target.classList = "glyphicon glyphicon-star";
+    });
+  }
+});
