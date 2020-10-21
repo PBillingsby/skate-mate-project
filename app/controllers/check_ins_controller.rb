ca5c96a5cff588852ab16193c493e5d0e8c68fa7
PBillingsby <philbillingsby@gmail.com>
2020-10-21 12:53:31 -0700

    Fix geocoder to get user location

--- app/controllers/check_ins_controller.rb
+++ app/controllers/check_ins_controller.rb
@@ -1,8 +1,6 @@
 class CheckInsController < ApplicationController
   include UsersHelper
   def create
-    # geocode_address 
-    byebug
     current_user.check_ins.create(location_id: @check_in_location.id)
     current_user_path
   end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,12 @@
 module UsersHelper
   def unique_user_check_ins
-    check_ins = current_user.check_ins.each {|x| x.location.city}
-    @unique_user_check_ins = check_ins.uniq {|x| x.location.city} # Creates an array of check_ins by unique city
+    check_ins = current_user.check_ins.each {|checkin| checkin.location.city}
+    @unique_user_check_ins = check_ins.uniq {|uniq_checkin| uniq_checkin.location.state} # Creates an array of check_ins by unique city
   end
 
   def user_location
     if last_check_in # Instance variable for last user check in
-      link_to "#{last_check_in.location.city}, #{last_check_in.location.country}", location_path(last_check_in.location)
+      link_to "#{last_check_in.location.city}, #{last_check_in.location.state}, #{last_check_in.location.country}", location_path(last_check_in.location)
     else
       "...nowhere. Check in below to add your location" # Prompts user to add user#location if no check_in 
     end
@@ -21,26 +21,8 @@ module UsersHelper
   end
 
   def geocode_address
-    byebug
-  #   location = Geocoder.search([request.location.latitude, request.location.longitude])
-  #   byebug
-  #   if location
-  #     current_user.check_ins.create(location_id: location.id)
-  #   end
-  #   #   flash[:error] = "Search can't be empty. Try again"
-  #   #   current_user_path
-  #   # end
-  #   # if check_in_params[:city].blank?
-  #   #   flash[:error] = "Search can't be empty. Try again"
-  #   #   current_user_path
-  #   # else
-  #   #   check_in_search = Geocoder.search(check_in_params[:city]).first # Finds first instance from city search
-  #   #   if !check_in_search
-  #   #     flash[:alert] = "No such place. Please try again"
-  #   #     @check_in_location = last_check_in
-  #   #   else
-  #   #     @check_in_location = Location.find_or_create_by(city: check_in_params[:city].titleize, country: check_in_search.country)
-  #   #   end
-  #   # end 
+    location = Geocoder.search([request.location.latitude, request.location.longitude])
+    user_location = current_user.locations.find_or_create_by(city: location.first.city, state: location.first.state, country: location.first.country)
+    CheckIn.create(user_id: current_user, location_id: user_location.id)
   end
 end
